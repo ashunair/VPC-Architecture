353c48bb51d3757e172c0964a77e1e8cb95448cf
ashunair <ashwathynair1998@gmail.com>
2025-01-23 15:26:45 -0500

    script.sh file instructions modified

--- VPC Architecture/script.sh
+++ VPC Architecture/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum update -y
-aws s3 cp s3://ed-web-config-project/index.html /var/www/html/
-systemctl restart httpd
-sudo yum install awslogs -y
+sudo apt update && sudo apt upgrade -y
+aws s3 cp s3://web-config-project/index.html /var/www/html/
+sudo systemctl restart httpd
+sudo apt install awslogs -y
 sudo systemctl start awslogsd
