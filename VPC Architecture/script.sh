211d2f57cc6ed3db4f7e50f9e8b1f9fdcc41099a
ashunair <ashwathynair1998@gmail.com>
2025-01-26 22:51:40 -0500

    changes in script.sh

--- VPC Architecture/script.sh
+++ VPC Architecture/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-aws s3 cp s3://web-config-project/index.html /var/www/html/
+sudo yum update -y
+aws s3 cp s3://portfolio-web-static-site-bucket/html-web-app/ /var/www/html/
 sudo systemctl restart httpd
-sudo apt install awslogs -y
+sudo yum install awslogs -y
 sudo systemctl start awslogsd
