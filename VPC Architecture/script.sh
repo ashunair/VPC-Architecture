3316ba1431356d82e28001c6c4a53148b28c53c3
ashunair <ashwathynair1998@gmail.com>
2025-01-27 01:16:56 -0500

    changes in script.sh

--- VPC Architecture/script.sh
+++ VPC Architecture/script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 sudo yum update -y
-aws s3 cp s3://portfolio-web-static-site-bucket/html-web-app/ /var/www/html/
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo aws s3 cp s3://portfolio-web-static-site-bucket/html-web-app/ /var/www/html/ --recursive
 sudo systemctl restart httpd
-sudo yum install awslogs -y
-sudo systemctl start awslogsd
+sudo yum install amazon-cloudwatch-agent -y
+# Start and enable the Amazon CloudWatch Agent
+sudo systemctl start amazon-cloudwatch-agent
+sudo systemctl enable amazon-cloudwatch-agent
